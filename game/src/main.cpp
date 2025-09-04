495e83bd357d6a4525c620af09ec4baeb60c4744
Felipe Rolon <afra.9622@gmail.com>
2025-09-04 11:56:29 -0400

    Lab 1 circle moving

--- game/src/main.cpp
+++ game/src/main.cpp
@@ -2,28 +2,53 @@
 #include "raymath.h"
 #define RAYGUI_IMPLEMENTATION
 #include "raygui.h"
+#include "game.h"
 
 const unsigned int TARGET_FPS = 50;
 float time = 0;
+float dt;
+float x = 500, y = 500;
+float frequency = 1.0f;
+float amplitude = 70;
+
+//Add sliders for frequency, amplitude and fps
+
+void update() {
+    dt = 1.0/TARGET_FPS;
+    time += dt;
+
+	x = x + (-sin(time * frequency)) * frequency * amplitude * dt;
+	y = y + (cos(time * frequency)) * frequency * amplitude * dt;
+}
+
+void draw() {
+    BeginDrawing();
+    ClearBackground(WHITE);
+    DrawText("Felipe Rolon - 101538323", 10, 10, 20, LIGHTGRAY);
+
+    GuiSliderBar(Rectangle{ 60, 5, 1000, 10 }, "Time", TextFormat("%.2f", time), &time, 0, 240);
+    DrawText(TextFormat("FPS: %i TIME: %.2f", TARGET_FPS, time), GetScreenWidth()-250, 30, 20, LIGHTGRAY);
+
+	DrawCircle(x, y, 60, RED);
+    DrawCircle(GetScreenWidth()/2 + cos((time * frequency) * amplitude), GetScreenHeight() / 2 + sin((time * frequency) * amplitude), 60, GREEN);
+
+    EndDrawing();
+}
+
 int main()
 {
-    InitWindow(1200, 800, "Physics-1");
+    InitWindow(InitialWidth, InitialHeight, "Physics Labs: Felipe Rolon 101538323");
     SetTargetFPS(TARGET_FPS);
 
     while (!WindowShouldClose())
     {
-        BeginDrawing();
-            ClearBackground(WHITE);
-            DrawText("Hello world!", 10, 10, 20, LIGHTGRAY);
-
-
-            time += 1;
-            GuiSliderBar(Rectangle{ 60, 5, 1000, 10 }, "Time", TextFormat("%.2f", time), &time, 0, 240);
-
-
-        EndDrawing();
+		update();
+		draw();
     }
 
     CloseWindow();
     return 0;
 }
+
+
+        
