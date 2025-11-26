bb189a3be35ef370a1e8036aa3b3b6128966b01c
Felipe Rolon <afra.9622@gmail.com>
2025-11-26 01:51:48 -0500

    Bouncing added lab 5

--- game/src/main.cpp
+++ game/src/main.cpp
@@ -12,6 +12,8 @@ struct birdObject {
     float drag = 1.0f;
     float mass = 1.0f;
 
+    bool fixed = false;
+
     float radius = 10.0f;
     Color color = GREEN;
 	Color initColor = GREEN;
@@ -22,7 +24,6 @@ struct halfSpace
     Vector2 position;
     Vector2 normal = { 0, -1 };
     float rotation = 0;
-
     Color color = BLUE;
     Color initColor = BLUE;
 
@@ -38,6 +39,7 @@ struct physicsWorld
 {
     Vector2 gravity = { 0.0f, 9.81f };
     std::vector<birdObject> entities;
+    std::vector<halfSpace> halfspaces; //vector to hold multiple walls
 };
 
 // detect collisions function
@@ -48,12 +50,68 @@ bool isColliding(const birdObject& a, const birdObject& b)
     return distance <= radiusSum;
 }
 
+bool isCollidingHS(const birdObject& circle, const halfSpace& hs)
+{
+    Vector2 displacement = circle.position - hs.position;
+    float dot = Vector2DotProduct(displacement, hs.normal);
+
+    // Overlap when dot < radius
+    return dot < circle.radius;
+}
+
+bool birdToBird(birdObject& a, birdObject& b)
+{
+    Vector2 diff = Vector2Subtract(a.position, b.position);
+    float distance = Vector2Length(diff);
+    float radiusSum = a.radius + b.radius;
+    if (distance >= radiusSum) return false;
+
+    float penetration = radiusSum - distance;
+
+    Vector2 normal = (distance > 0) ? Vector2Scale(diff, 1.0f / distance) : Vector2{ 0, 1 }; //if distance is 0 pick arbitrary Y direction
+    Vector2 mtv = Vector2Scale(normal, penetration);
+
+    // Caso 1 if both are fixed, do nothing
+    if (a.fixed && b.fixed) return true;
+
+    // Caso 2 If A is fixed
+    if (a.fixed) {
+        b.position = Vector2Subtract(b.position, mtv);
+    }
+    // Caso3 if B is fixed
+    else if (b.fixed) {
+        a.position = Vector2Add(a.position, mtv);
+    }
+    // Caso 4 Both aren't fixed
+    else {
+        Vector2 halfMtv = Vector2Scale(mtv, 0.5f);
+        a.position = Vector2Add(a.position, halfMtv);
+        b.position = Vector2Subtract(b.position, halfMtv);
+    }
+
+    return true;
+}
+
+bool birdToHS(birdObject& circle, const halfSpace& hs)
+{
+    Vector2 toCircle = Vector2Subtract(circle.position, hs.position);
+    float dot = Vector2DotProduct(toCircle, hs.normal);
+    if (dot < circle.radius)
+    {
+        float penetration = circle.radius - dot;
+        circle.position = Vector2Add(circle.position, Vector2Scale(hs.normal, penetration));
+        return true;
+    }
+    return false;
+}
+
+
 const unsigned int TARGET_FPS = 60;
 float time = 0;
 float dt;
 
 physicsWorld world;
-halfSpace halfspace;
+//halfSpace halfspace;
 
 
 //float birdRadius = 10.0f;
@@ -82,7 +140,24 @@ void update() {
         bird.velocity += world.gravity * dt;
         bird.velocity *= powf(bird.drag, dt);
         bird.position += bird.velocity * dt;
-		bird.color = bird.initColor;
+        /*if (isCollidingHS(bird, halfspace))
+        {
+            bird.color = BROWN;
+            halfspace.color = BROWN;
+        }*/
+        for (size_t j = 0; j < world.halfspaces.size(); j++) {
+			halfSpace& halfspace = world.halfspaces[j];
+            if (birdToHS(bird, halfspace)) {
+                bird.color = BROWN;
+                halfspace.color = BROWN;
+            }
+            else
+            {
+                halfspace.color = halfspace.initColor;
+                bird.color = bird.initColor;
+            }
+        }   
+        
     }
     for (size_t i = 0; i < world.entities.size(); i++)
     {
@@ -90,7 +165,12 @@ void update() {
         {
             birdObject& bird = world.entities[i];
             birdObject& birdTwo = world.entities[j];
-            if (isColliding(bird, birdTwo))
+            /*if (isColliding(bird, birdTwo))
+            {
+                bird.color = RED;
+                birdTwo.color = RED;
+            }*/
+            if (birdToBird(bird, birdTwo))
             {
                 bird.color = RED;
                 birdTwo.color = RED;
@@ -100,6 +180,7 @@ void update() {
 }
 
 void drawHalfspace() {
+	halfSpace& halfspace = world.halfspaces[0];
     // Draw circle
     DrawCircleV(halfspace.position, 8, halfspace.color);
 
@@ -151,10 +232,24 @@ void draw() {
 	DrawText(TextFormat("Launch Angle: %.2f", launchAngle), 10, 50, 20, LIGHTGRAY);
 	DrawText(TextFormat("Launch Speed: %.2f", launchSpeed), 10, 70, 20, LIGHTGRAY);
 
+	//Draw halfspaces
+    for (const halfSpace& hs : world.halfspaces) {
+        DrawLineEx(hs.position, Vector2Add(hs.position, Vector2Scale(hs.normal, 40)), 3, hs.color);
+        Vector2 tangent = Vector2Rotate(hs.normal, PI * 0.5f);
+        DrawLineEx(
+            Vector2Subtract(hs.position, Vector2Scale(tangent, 1000)),
+            Vector2Add(hs.position, Vector2Scale(tangent, 1000)),
+            4, hs.color
+        );
+
+        DrawCircleV(hs.position, 5, hs.color);
+    }
+
 	// Draw all bird objects
     for (const birdObject& e : world.entities)
     {
-        DrawCircleV(e.position, e.radius, e.color);
+        Color drawColor = e.fixed ? BLACK : e.color;
+        DrawCircleV(e.position, e.radius, drawColor);
     }
 
     float previewScale = 0.5f;
@@ -164,8 +259,32 @@ void draw() {
     DrawLineEx(launchPosition, previewTip, 4.0f, RED);
 
 	drawHalfspace();
+    DrawText("Press SPACE to shot a bird", 400, 700, 20, DARKGRAY);
+    DrawText("Press F to spawn a fixed bird", 400, 730, 20, DARKGRAY);
 
     EndDrawing();
+}
+
+void drawRamps() {
+    // 1. Floor
+    halfSpace floor;
+    floor.position = { 500, 780 };
+    floor.SetRotation(0);
+    world.halfspaces.push_back(floor);
+
+    //// 2. Left Ramp
+    //halfSpace leftRamp;
+    //leftRamp.position = { 200, 500 };
+    //leftRamp.SetRotation(-30);
+    //leftRamp.color = DARKBLUE;
+    //world.halfspaces.push_back(leftRamp);
+
+    //// 3. Right Ramp
+    //halfSpace rightRamp;
+    //rightRamp.position = { 800, 500 };
+    //rightRamp.SetRotation(30);
+    //rightRamp.color = MAROON;
+    //world.halfspaces.push_back(rightRamp);
 }
 
 
@@ -173,8 +292,10 @@ int main()
 {
     InitWindow(InitialWidth, InitialHeight, "Physics Labs: Felipe Rolon 101538323");
     SetTargetFPS(TARGET_FPS);
-    halfspace.position = { 500, 600 };
-    halfspace.SetRotation(0);
+
+    drawRamps();
+    /*halfspace.position = { 500, 600 };
+    halfspace.SetRotation(0);*/
 
     while (!WindowShouldClose())
     {
@@ -191,6 +312,19 @@ int main()
                 (unsigned char)(rand() % 256),
                 255
             };
+            bird.fixed = false;
+            world.entities.push_back(bird);
+        }
+
+        if (IsKeyPressed(KEY_F))
+        {
+            birdObject bird;
+            bird.position = launchPosition;
+            bird.velocity = { 0,0 };
+            bird.radius = 30.0f;
+            bird.initColor = BLACK;
+            bird.color = BLACK;
+            bird.fixed = true;
             world.entities.push_back(bird);
         }
 		draw();
